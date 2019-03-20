b2b19ba6f2ee13ab6c8e4f9d6fe77226d61efdca
若山侑輝 <wkym_4a@icloud.com>
2019-03-20 12:09:58 +0900

    とりあえずトップ画面の表示だけ可能に

--- config/application.rb
+++ config/application.rb
@@ -4,13 +4,13 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-#使用する「時間」を東京のものに
-config.time_zone = 'Tokyo'
-config.active_record.default_timezone = :local
 
 module FaceClone
   class Application < Rails::Application
     config.load_defaults 5.1
 
+    #使用する「時間」を東京のものに
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
